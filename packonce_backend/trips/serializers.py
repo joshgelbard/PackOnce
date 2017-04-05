74041c88afdcf6adebd5191db7e4827d81756a90
Jonathon Wright <jonwright93@gmail.com>
2017-04-04 19:27:13 -0700

    update backend key and update serializations

--- packonce_backend/trips/serializers.py
+++ packonce_backend/trips/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-from trips.models import Trips
+from trips.models import Trip
 
-class TripSerializer(serializers.Serializer)
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False max_length=100)
-    status = serializers.CharField(required=True, max_length= 20)
-    date_range = serializers.CharField(required=True, max_length=100);
-
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ('id', 'name', 'status', 'date_range')
     def create(self, validated_data):
         return Trips.objects.create(**validated_data)
 
