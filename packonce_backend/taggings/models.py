afb5531b65db51266574b90bbe08932fcccbde2d
Jonathon Wright <jonwright93@gmail.com>
2017-04-08 15:35:27 -0700

    seed initial data

--- packonce_backend/taggings/models.py
+++ packonce_backend/taggings/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Tagging(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    # created = models.DateTimeField(auto_now_add=True)
     item = models.CharField(max_length=250, blank=True)
     activity = models.CharField(max_length=250, blank=True)
     category = models.CharField(max_length=250, default='other')
