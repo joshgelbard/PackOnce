61436acafb07ff490d56c89f80a837af911003bd
Qadir Mohammed-Ali <a.mohammedali@outlook.com>
2017-04-09 00:13:11 -0700

    Redo folder structure

--- packBack/urls.py
+++ packBack/urls.py
@@ -1,4 +1,4 @@
-"""packonce_backend URL Configuration
+"""packBack URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,6 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+# from django.conf.urls import url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
